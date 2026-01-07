9339e0f9a6e57bcbbfdaca7f8ed970587a2cb9f7
Andrey Yurchuk <r1430@tut.by>
2026-01-07 20:38:54 +0300

    feat: implement getFirstResolvedPromiseResult and getFirstPromiseResult functions

--- src/core-js-promises-tasks.js
+++ src/core-js-promises-tasks.js
@@ -55,8 +55,12 @@ function getPromiseResult(source) {
  * [Promise.resolve(1), Promise.reject(2), Promise.resolve(3)]  => Promise fulfilled with 1
  * [Promise.reject(1), Promise.reject(2), Promise.reject(3)]    => Promise rejected
  */
-function getFirstResolvedPromiseResult(/* promises */) {
-  throw new Error('Not implemented');
+async function getFirstResolvedPromiseResult(promises) {
+  try {
+    return await Promise.any(promises);
+  } catch {
+    return Promise.reject(promises);
+  }
 }
 
 /**
@@ -78,8 +82,8 @@ function getFirstResolvedPromiseResult(/* promises */) {
  * [promise3, promise6, promise2] => Promise rejected with 2
  * [promise3, promise4, promise6] => Promise rejected with 6
  */
-function getFirstPromiseResult(/* promises */) {
-  throw new Error('Not implemented');
+function getFirstPromiseResult(promises) {
+  return Promise.race(promises);
 }
 
 /**
